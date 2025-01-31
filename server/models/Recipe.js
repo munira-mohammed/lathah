cd8ed06fcfb7ed8888d81a31cf21127fe05a0389
munira-mohammed <148888231+munira-mohammed@users.noreply.github.com>
2025-01-31 16:17:00 +0300

    add some entity in recipe schema

--- server/models/Recipe.js
+++ server/models/Recipe.js
@@ -1,24 +1,62 @@
 const mongoose = require('mongoose');
+const mongoose = require("mongoose");
+const { authorize } = require("passport");
+const Schema = mongoose.Schema; 
 
 const recipeSchema = new mongoose.Schema({
   name: {
     type: String,
-    required: 'This field is required.'
+    required: true, // اسم الوصفة
   },
-
-  email: {
+  authorize: {
     type: String,
-    required: 'This field is required.'
-  },
-  ingredients: {
-    type: Array,
-    required: 'This field is required.'
+    required: true, 
   },
   image: {
+    type: String, // رابط الصورة (يتم تخزين مسار الصورة بعد رفعها)
+  },
+  type: {
     type: String,
-    required: 'This field is required.'
+    enum: ["أطباق رئيسية", "مقبلات", "مشروبات", "حلويات"], // نوع الطبق باللغة العربية
+    required: true,
+  },
+  level: {
+    type: String,
+    enum: ["صعب", "متوسط", "سهل"], // مستوى الصعوبة باللغة العربية
+    required: true,
+  },
+  time: {
+    type: Number, // الوقت بالدقائق
+    required: true,
+  },
+  minPeople: {
+    type: Number, // الحد الأدنى لعدد الأشخاص
+    required: true,
+  },
+  maxPeople: {
+    type: Number, // الحد الأقصى لعدد الأشخاص
+    required: true,
+  },
+  ingredients: {
+    type: [String], // مصفوفة من النصوص (المكونات)
+    required: [true, 'This field is required.'], // إلزامية المكونات
+  },
+  steps: 
+    {
+      type: [String], // مصفوفة من النصوص (المكونات)
+    required: [true, 'This field is required.'], // إلزامية المكونات
+    },
+  
+  createdAt: {
+    type: Date,
+    default: Date.now, // تاريخ الإنشاء
+  },
+  user: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "User",
+    required: true
   },
 });
 
 
-module.exports = mongoose.model('Recipe', recipeSchema);
+module.exports = mongoose.model("Recipe", recipeSchema);
